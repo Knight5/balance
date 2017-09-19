e14b2f5a15aad21ee4346e6a4810a612b09c99aa
Knight5 <Knight5@users.noreply.github.com>
2017-09-18 21:27:54 -0700

    Keeping up with the Green, trying to share between systems

---
+++ main.cpp
@@ -0,0 +1,38 @@
+//
+//  main.cpp
+//  event1
+//
+//  Created by Scott Kelly on 9/1/15.
+//  Copyright (c) 2015 Scott Kelly. All rights reserved.
+//
+
+
+#include <iostream>
+using namespace std;
+
+
+#define YUP 25
+//int argc, const char * argv[]
+//std::cout << "Another World\n";
+
+int main() {
+    char cha;
+    int num1;
+    int num2;
+    cout << "Begin";
+    cin >> cha;
+    cout << cha << "\n";
+    cout << "What number1?";
+    cin >> num1;
+    cout << "What number2?";
+    cin >> num2;
+    cout << num1 +num2 << "\n";
+    cout << num1 %num2 << "\n";
+    
+    // cin.get();  //program wait; // q to exit
+    
+    cout << "fin";
+    
+    //second(){}// mod is the remainder
+    return 1;
+}
